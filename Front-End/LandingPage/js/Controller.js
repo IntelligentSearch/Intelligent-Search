80c50062e5634e7aab0a648b0329d08369279b3b
Apoorva Parmar <parmar2@purdue.edu>
2016-10-04 12:33:50 -0400

    Fetched the data from the Purdue API to the front end and displayed them on the cards for each dining court!

--- Front-End/LandingPage/js/Controller.js
+++ Front-End/LandingPage/js/Controller.js
@@ -5,16 +5,36 @@
 (function() {
     var myApp = angular.module('BlankApp', ['ngMaterial']);
 
-    myApp.config(['$httpProvider', function($httpProvider) {
+    myApp.config(['$httpProvider', '$mdThemingProvider', function($httpProvider, $mdThemingProvider) {
         $httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.withCredentials = true;
         delete $httpProvider.defaults.headers.common["X-Requested-With"];
         $httpProvider.defaults.headers.common["Accept"] = "application/json";
         $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amber', {
+                'default': 'A700', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': 'A700', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': 'A700', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A700' // use shade A100 for the <code>md-hue-3</code> class
+            })
+            .accentPalette('orange', {
+                'default': 'A700', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': 'A700', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': 'A700', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A700' // use shade A100 for the <code>md-hue-3</code> class
+            });
     }]);
 
     myApp.controller('MyController', ['$scope', '$http', '$timeout', '$mdSidenav', '$log', function($scope, $http, $timeout, $mdSidenav, $log) {
-        $scope.fuckthis = 'Hola!';
+
+        $scope.showSpinner = true;
+        var offset = -5.0;
+
+        var clientDate = new Date();
+        var utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
+
+        $scope.selectedDate = new Date(utc + (3600000*offset));
 
         $scope.toggleLeft = buildDelayedToggler('left');
         /**
@@ -63,6 +83,7 @@
 
         var imagePath = 'img/list/60.jpeg';
 
+
         $scope.toppings = [
             {name: 'Scrambled Egg', wanted: true},
             {name: 'Sausage', wanted: false},
@@ -83,13 +104,42 @@
         $scope.tabs = tabs;
         $scope.selectedIndex = 0;
         $scope.$watch('selectedIndex', function(current, old){
+            $scope.getMealData();
             previous = selected;
             selected = tabs[current];
             if ( old + 1 && (old != current)) $log.debug('Goodbye ' + previous.title + '!');
             if ( current + 1 )                $log.debug('Hello ' + selected.title + '!');
         });
 
-        
+        $scope.$watch('selectedDate', function() {
+           $scope.getMealData();
+        });
+
+        $scope.getMealData = function() {
+            $scope.showSpinner = true;
+
+            var stringDate = $scope.selectedDate.toISOString().substring(0, 10);
+            stringDate = stringDate.substring(5, 7) + "-" + stringDate.substring(8, 10) + "-" + stringDate.substring(0,4);
+
+            $http({
+                url: "https://api.hfs.purdue.edu/menus/v1/locations/" + tabs[$scope.selectedIndex].title + "/" + stringDate,
+                method: "GET"
+            }).success(function(data, status, headers, config) {
+                if(data != null) {
+                    $scope.mealData = data;
+                }
+
+                $timeout(function() {
+                    $scope.showSpinner = false;
+                }, 2000);
+
+
+            }).error(function (data, status, headers, config) {
+                $scope.showSpinner = false;
+            });
+        };
+
+        $scope.getMealData();
 
 
     }]).controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
