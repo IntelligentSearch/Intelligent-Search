871e967e4979fb3f6e8704c1bef3ed8e323def2a
meyeralex31 <meyer104@purdue.edu>
2016-10-09 21:59:46 -0400

    update working maybe?

--- Back-End/src/main/java/com/rest/Call.java
+++ Back-End/src/main/java/com/rest/Call.java
@@ -24,7 +24,7 @@ public class Call {
         		while (res.next()) {
         			String name = res.getString("I.NAME");
 					String station = res.getString("D.STATION");
-					String  id = res.getString("I.Item_ID");
+					String  id = res.getString("D.Item_ID");
 					boolean breakfast = res.getBoolean("D.BREAKFAST");
 					boolean lunch = res.getBoolean("D.LUNCH");;
 					boolean dinner = res.getBoolean("D.DINNER");
@@ -37,7 +37,7 @@ public class Call {
         			i.setBreakfast(breakfast);
         			i.setLunch(lunch);
         			i.setDinner(dinner);
-        			i.setAllergens(Helper.getAllergens(id));
+        			i.setAllergens(pref);
         			Helper.getNutrition(id,i);
         			ja.put(i.processItem());
         		}
@@ -48,5 +48,93 @@ public class Call {
 				System.out.println("\n"+e.toString());
 			}
 			return ja;			
+		}
+		public static JSONArray getItemDin(String diningCourt, String item) {
+			JSONArray ja = new JSONArray();
+			Connection con;
+			PreparedStatement prep_stmt;
+			Statement stmt;
+			ResultSet res;
+			//sets up connection
+			Connection conn;
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DINING", "root", "cz002");
+				String query = "SELECT I.Item_ID, I.NAME,D.STATION,D.BREAKFAST,D.LUNCH,D.DINNER "
+					+	"FROM Daily AS D "
+					+	"JOIN Item AS I "
+					+	"ON D.ITEM_ID = I.Item_ID "
+					+	"WHERE I.NAME = ? AND D.LOCATION = ?";
+				prep_stmt = conn.prepareStatement(query);
+				prep_stmt.setString(1, item);
+				prep_stmt.setString(2,diningCourt);
+				res = prep_stmt.executeQuery();
+				if (res.next()){
+					String name = res.getString("I.NAME");
+					String station = res.getString("D.STATION");
+					String  id = res.getString("I.Item_ID");
+					boolean breakfast = res.getBoolean("D.BREAKFAST");
+					boolean lunch = res.getBoolean("D.LUNCH");;
+					boolean dinner = res.getBoolean("D.DINNER");
+					Item i = new Item(name,id,station,diningCourt);
+        			i.setBreakfast(breakfast);
+        			i.setLunch(lunch);
+        			i.setDinner(dinner);
+        			i.setAllergens(Helper.getAllergens(id));
+        			Helper.getNutrition(id,i);
+        			ja.put(i.processItem());
+				}
+				else {
+					
+				}
+				
+				res.close();
+     			prep_stmt.close();
+     			conn.close();
+			}
+			catch (Exception e) {
+				System.out.println("\n"+e.toString());
+			}
+			return ja;
+		}
+		public static JSONArray getItem(String item) {
+			JSONArray ja = new JSONArray();
+			Connection con;
+			PreparedStatement prep_stmt;
+			Statement stmt;
+			ResultSet res;
+			//sets up connection
+			try{
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/DINING", "root", "cz002");
+				String query = "SELECT D.LOCATION, D.ITEM_ID, I.Item_ID,D.STATION,D.BREAKFAST,D.LUNCH,D.DINNER "
+						+	"FROM Daily AS D, Item AS I "
+						+	"WHERE D.ITEM_ID = (SELECT I.Item_ID  "
+						+	"WHERE I.NAME = ?)";
+				prep_stmt = con.prepareStatement(query);
+				prep_stmt.setString(1, item);
+				res = prep_stmt.executeQuery();
+				while(res.next()) {
+					String name = item;
+					String diningCourt = res.getString("D.LOCATION");
+					String station = res.getString("D.STATION");
+					String id = res.getString("I.Item_ID");
+					boolean breakfast = res.getBoolean("D.BREAKFAST");
+					boolean lunch = res.getBoolean("D.LUNCH");;
+					boolean dinner = res.getBoolean("D.DINNER");
+					Item i = new Item(name,id,station,diningCourt);
+        			i.setBreakfast(breakfast);
+        			i.setLunch(lunch);
+        			i.setDinner(dinner);
+        			i.setAllergens(Helper.getAllergens(id));
+        			Helper.getNutrition(id,i);
+        			ja.put(i.processItem());
+				}
+				res.close();
+     			prep_stmt.close();
+     			con.close();
+			}
+			catch (Exception e) {
+				System.out.println("\n"+e.toString());
+			}
+			return ja;
 		}
 }
