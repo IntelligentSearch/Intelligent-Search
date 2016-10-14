50ae9e1ad4080c1b04dbcd220c437151f9d8b6e5
vieck <vieck@purdue.edu>
2016-10-14 13:41:55 -0400

    Fixed register ng-models and requests are working but returning error 500

--- app/login/login.js
+++ app/login/login.js
@@ -2,7 +2,11 @@
 
 var app = angular.module('myApp.login', ['ngRoute'])
 
-    .config(['$routeProvider', function ($routeProvider) {
+    .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common["X-Requested-With"];
+        $httpProvider.defaults.headers.common["Accept"] = "application/json";
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
         $routeProvider.when('/login', {
             templateUrl: 'app/login/login.html',
             controller: 'LoginCtrl',
@@ -17,28 +21,29 @@ var app = angular.module('myApp.login', ['ngRoute'])
 
         $scope.base_url = "http://cs307.cs.purdue.edu:8080/home/cs307/Intelligent-Search/Back-End/target/Back-End/rest";
 
-        $scope.auth = function() {
-                // GET request for logging into an account:
-                $http({
-                    method: 'POST',
-                    url: $scope.base_url + '/login',
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                    transformRequest: function(obj) {
-                        var str = [];
-                        for(var p in obj)
-                            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                        return str.join("&");
-                    },
-                    data: {name: $scope.user, password: $scope.password}
-                }).then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    $location.path("/dining");
+        $scope.auth = function () {
+            // GET request for logging into an account:
+            $http({
+                method: 'POST',
+                url: $scope.base_url + '/login',
+                withCredentials: true,
+                // headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                transformRequest: function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                data: {name: $scope.user.userName, password: $scope.user.password}
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                $location.path("/dining");
 
-                }, function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    alert('Login failed' + angular.toJson(response));
-                });
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                alert('Login failed' + angular.toJson(response));
+            });
         }
     });
--- app/register/register.js
+++ app/register/register.js
@@ -2,7 +2,11 @@
 
 var app = angular.module('myApp.register', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
+.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
   $routeProvider.when('/register', {
     templateUrl: 'app/register/register.html',
     controller: 'RegisterCtrl',
@@ -13,7 +17,7 @@ var app = angular.module('myApp.register', ['ngRoute'])
 
     $scope.base_url = "http://cs307.cs.purdue.edu:8080/home/cs307/Intelligent-Search/Back-End/target/Back-End/rest";
 
-    $scope.new_user = {
+    $scope.account = {
       first: '',
       last: '',
       email: '',
@@ -23,20 +27,15 @@ var app = angular.module('myApp.register', ['ngRoute'])
     $scope.createAccount = function() {
       // POST request for creating an account:
       $http({
-        method: 'GET',
-        url: $scope.base_url + '/create-user/',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-        },
-        data: {name: $scope.new_user.email, password: $scope.new_user.password, first: $scope.new_user.first, last: $scope.new_user.last}
+        method: 'POST',
+        url: $scope.base_url + '/create-user',
+        withCredentials: true,
+        // headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        data: {name: $scope.account.email, password: $scope.account.password, first: $scope.account.first, last: $scope.account.last}
       }).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-      }, function errorCallback(response) {
+        }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
 
