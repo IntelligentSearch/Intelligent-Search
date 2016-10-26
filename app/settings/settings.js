42f238b42dc1baa3cbcaed43608253460ab323f6
vieck <vieck@purdue.edu>
2016-10-26 10:36:03 -0400

    Set User Name works, true and false updating with angular works

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -16,13 +16,14 @@ var app = angular.module('myApp.settings', ['ngRoute', 'ngCookies'])
     .controller('SettingsCtrl', function ($scope, $location, $http, $cookies) {
         var response = $cookies.getObject('user');
         console.log(response);
+        $scope.base_url = "http://cs307.cs.purdue.edu:8080/home/cs307/Intelligent-Search/Back-End/target/Back-End/rest";
         if (response != undefined) {
             $scope.userID = response.user.UserID;
             $scope.preferences = response.prefs;
             angular.forEach($scope.preferences, function (value, key) {
-               console.log(key + ' : '+ value)
+                console.log(key + ' : ' + value)
             });
-            console.log("Prefs: " +response.prefs);
+            console.log("Prefs: " + response.prefs);
             $scope.name = {
                 firstName: response.user.FirstName,
                 lastName: response.user.LastName
@@ -32,12 +33,15 @@ var app = angular.module('myApp.settings', ['ngRoute', 'ngCookies'])
                 oldPassword: '',
                 newPassword: ''
             }
+            console.log($scope.base_url);
 
 
-            $scope.base_url = "http://cs307.cs.purdue.edu:8080/home/cs307/Intelligent-Search/Back-End/target/Back-End/rest";
-
-            $scope.auth = function ($scope, $http, $timeout, $location, $log, $cookies) {
-
+        }
+        $scope.auth = function () {
+            console.log("user: " + $scope.userID);
+            if (($scope.name.firstName != undefined && $scope.name.lastName != undefined)
+            && (response.user.FirstName != $scope.name.firstName || response.user.LastName != $scope.name.lastName)) {
+                console.log("Changing name");
                 //POST request for updating name
                 $http({
                     method: 'POST',
@@ -49,12 +53,18 @@ var app = angular.module('myApp.settings', ['ngRoute', 'ngCookies'])
                         newLast: $scope.name.lastName
                     })
                 }).then(function successCallback(response) {
-
+                    var response = $cookies.getObject('user');
+                    response.user.FirstName = $scope.name.firstName;
+                    response.user.LastName = $scope.name.lastName;
+                    $cookies.putObject('user', response);
                 }, function errorCallback(response) {
                     alert('Updating name failed' + angular.toJson(response));
                 });
-
-                //POST request for updating password
+            }
+            console.log("user: " + $scope.userID);
+            //POST request for updating password
+            if ($scope.password.newPassword != undefined && $scope.password.oldPassword) {
+                console.log("Changing password");
                 $http({
                     method: 'POST',
                     url: $scope.base_url + '/update-password',
@@ -70,29 +80,26 @@ var app = angular.module('myApp.settings', ['ngRoute', 'ngCookies'])
                     alert('Settings failed' + angular.toJson(response));
                 });
 
-                // POST request for setting preferences
-                $http({
-                    method: 'POST',
-                    url: $scope.base_url + '/set-pref',
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                    data: $.param({userID: $scope.userID, prefs: $scope.preferences})
-                }).then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    var userID = response.data.user.UserID;
-                    if (userID != -1) {
-                       // $cookies.put('user', userID);
-                        $cookies.put('user_name', $scope.user.userName);
-                        $location.path("/dining");
-                    } else {
-                        alert('Settings failed');
-                    }
-
-                }, function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    alert('Settings failed' + angular.toJson(response));
-                });
             }
+            console.log("user: " + $scope.userID, "prefs: " + $scope.preferences);
+            // POST request for setting preferences
+            $http({
+                method: 'POST',
+                url: $scope.base_url + '/set-pref',
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                data: $.param({userID: $scope.userID, prefs: $scope.preferences})
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+               console.log(response);
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                alert('Settings failed' + angular.toJson(response));
+            });
+        }
+        $scope.onChange = function (index, key) {
+            $scope.preferences[key] = !$scope.preferences[key];
+            console.log(key + " " + $scope.preferences[key]);
         }
     });
